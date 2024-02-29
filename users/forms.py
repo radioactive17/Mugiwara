a216424e7d980113c80f563ddc3b648bd093b40a
Jignesh Nagda <jigneshnagda@Jigneshs-MacBook-Pro.local>
2024-02-28 23:04:08 -0500

    Resolved minor bugs

--- users/forms.py
+++ users/forms.py
@@ -9,13 +9,25 @@ class UserUpdateForm(ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'username', ]
+    
+    def __init__(self, *args, **kwargs):
+        super(UserUpdateForm, self).__init__(*args, **kwargs)
+
+        if self.instance.id:
+            self.fields['username'].disabled = True
 
 class BankingUserUpdateForm(ModelForm):
     class Meta:
         model = BankingUser
         fields = "__all__"
+        exclude = ['user']
         
+    def __init__(self, *args, **kwargs):
+        super(BankingUserUpdateForm, self).__init__(*args, **kwargs)
 
+        if self.instance.id:
+            self.fields['user_type'].disabled = True
+    
     
     
 
